fb73daa11301843e25775710a98a41530bd49b97
Frederick <frederick88223@gmail.com>
2022-01-28 10:51:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
 
+// var name = "";
+// let text = prompt("Hello.. What's your name?", "a");
+// name = text.toLowerCase();
+// while (name != "jasmine" || name != "jas" || name != "jasmine g" || name != "jasmine gouwandi"){
+//   text = prompt("Sadly this website is not for you. Type in again.")
+  
+// }
+
 $('#home').fadeIn();
 
 $(window).scroll(function (){
